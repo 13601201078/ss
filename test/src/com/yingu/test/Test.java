c93b228906e97fe0376427ef7146f5a43486bf1d
lilunlun <lilunlun@yingu.com>
2016-12-16 17:47:52 +0800

    测试

---
+++ test/src/com/yingu/test/Test.java
@@ -0,0 +1,14 @@
+package com.yingu.test;
+
+/**
+ * Created by Administrator on 2016/12/16.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        system.out.prrin("123444");
+    }
+
+
+
+}
